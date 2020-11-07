27350fb1ee6ed13ceebe8f0ca3371b3348c56674
Nathan <nate1898@gmail.com>
2020-11-06 22:41:20 -0500

    Finished Mvp

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,18 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    destinations = {}
+    route = []
+
+    for i in range(length):
+        destinations[tickets[i].source] = tickets[i].destination
+
+    for i in range(length):
+        if i == 0:
+            route.append(destinations["NONE"])
+        else:
+            route.append(destinations[route[i-1]])
+
+    print(destinations)
 
     return route
